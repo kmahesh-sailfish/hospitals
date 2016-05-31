3393fa453ac1e0eb6036de414892e0ceb02ab534
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-05-31 12:45:30 +0530

    app commit

--- app.js
+++ app.js
@@ -28,11 +28,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/',function(req,res){
   res.send('welcome to Api exprees..');
 
 });
+app.get('/patient',function(req,res){
+  pool.query('select * from tbl_patient',function(err,rows,fields){
+    if(!err){
+      res.send(rows[0]);
+    }
+    else
+    {
+      console.log('Error while performing Query');
+    }
+  })
+})
 app.get('/admin',function(req,res){
   pool.query("select* from usertypes",function(err,rows,fields){
     if(!err){
